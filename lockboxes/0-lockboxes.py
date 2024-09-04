923c6220e4f31b6f3584227c6cff4da14df5c9f2
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-09-04 16:59:31 -0400

    added newline to 0-lockboxes for pycodestyle

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -11,22 +11,16 @@ def canUnlockAll(boxes):
 
     boxes_opened_status = [False for _ in boxes]
     boxes_opened_status[0] = True
-
     while True:
-
         new_boxes_opened_status = boxes_opened_status.copy()
-
         for box_index, box_is_opened in enumerate(boxes_opened_status):
             if not box_is_opened:
                 continue
-
             for box_key in boxes[box_index]:
-
                 try:
                     new_boxes_opened_status[box_key] = True
                 except IndexError:
                     pass
-
         if new_boxes_opened_status == boxes_opened_status:
             break
 
